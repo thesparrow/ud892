b77307607c8b707a6a1a1f88f201237d001569c3
Anna <Anya@Annas-MacBook-Air.local>
2018-08-03 17:54:02 -0700

    Adding Lesson 3

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,10 +1,25 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
+
+
+// configure which files to watch and what tasks to use on file changes
 gulp.task('default', function() {
-	console.log('hello world!');
+	gulp.watch('sass/**/*.scss', gulp.series('styles'));
+	
+	browserSync.init({
+		server: "./"
+	});
+	browserSync.stream();
 });
+
+/*gulp.task('watch', function() {
+	gulp.watch('app/css/*.css', gulp.series('styles'));
+	gulp.watch('app/js/*.js', gulp.series('scripts'));
+	gulp.watch('app/img/*', gulp.series('images'));
+});*/
 
 gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
@@ -12,5 +27,7 @@ gulp.task('styles', function() {
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		}))
-		.pipe(gulp.dest('./css'));
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
 });
+
--- Lesson 3/gulpfile.js
+++ Lesson 3/gulpfile.js
@@ -1,22 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['styles'], function() {
-	gulp.watch('sass/**/*.scss', ['styles']);
-
-	browserSync.init({
-		server: './'
-	});
+ gulp.task('styles', function() {
+	    gulp.src('sass/*.scss')
+			.pipe(watch('sass/*.scss'))
+	        .pipe(sass().on('error', sass.logError))
+	        .pipe(autoprefixer({
+	            browsers: ['last 2 versions']
+	        }))
+	        .pipe(gulp.dest('./css'))
+	        .pipe(browserSync.stream());
 });
 
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'))
-		.pipe(browserSync.stream());
+//assign a default task that watches files
+gulp.task('default', function() {
+	    gulp.watch('sass/*.scss',gulp.series('styles') );
+	
+	    browserSync.init({
+	        server: './'
+	    });
 });
+
+
+//gulp.watch('sass/**/*.scss', gulp.series('styles'));
+gulp.watch("*.html").on('change', browserSync.reload);
